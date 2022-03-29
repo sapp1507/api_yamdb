e9db6a325e90c514e8d2dbab300bcb67d65f2e47
Yakov <93822070+potapovjakov@users.noreply.github.com>
2022-03-29 22:33:35 +0300

    Add CommentViewSet and ReviewViewSet

---
+++ api_yamdb/api/views.py
@@ -0,0 +1,37 @@
+from django.shortcuts import get_object_or_404
+from rest_framework import viewsets
+from rest_framework.pagination import LimitOffsetPagination
+from .permissions import IsAuthorOrReadOnly
+
+from reviews.models import (Review, Comment,)
+from .serializers import (ReviewSerializer, CommentSerializer,)
+
+
+class ReviewViewSet(viewsets.ModelViewSet):
+    queryset = Review.objects.all()
+    serializer_class = ReviewSerializer
+    permission_classes = [IsAuthorOrReadOnly]
+    pagination_class = LimitOffsetPagination
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
+
+
+class CommentViewSet(viewsets.ModelViewSet):
+    serializer_class = CommentSerializer
+    permission_classes = [IsAuthorOrReadOnly]
+
+    def get_queryset(self):
+        review_id = self.kwargs.get('review_id')
+        review = get_object_or_404(
+            Comment,
+            id=review_id
+        )
+        return review.comments.all()
+
+    def perform_create(self, serializer):
+        review = get_object_or_404(
+            Comment,
+            pk=self.kwargs.get('review_id')
+        )
+        serializer.save(author=self.request.user, review=review)
