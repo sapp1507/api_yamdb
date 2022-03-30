36f310909623c43c11c515bdee6751bc3e4eb37e
Yakov <93822070+potapovjakov@users.noreply.github.com>
2022-03-30 11:06:15 +0300

    Add check max_one_review (one title -one review)

--- api_yamdb/api/serializers.py
+++ api_yamdb/api/serializers.py
@@ -1,7 +1,9 @@
+from django.shortcuts import get_object_or_404
 from rest_framework import serializers
+from rest_framework.exceptions import ParseError
 from rest_framework.relations import SlugRelatedField
 
-from reviews.models import Comment, Review
+from reviews.models import Comment, Review, Title
 
 
 class CommentSerializer(serializers.ModelSerializer):
@@ -24,3 +26,15 @@ class ReviewSerializer(serializers.ModelSerializer):
     class Meta:
         fields = '__all__'
         model = Review
+
+    def max_one_review(self, data):
+        title_id = self.context["request"].parser_context["kwargs"]["title_id"]
+        title = get_object_or_404(Title, pk=title_id)
+        user = self.context["request"].user
+        if (
+            self.context["request"].method == "POST"
+            and Review.objects.filter(author=user, title=title).exists()
+        ):
+            raise ParseError("Нельзя оставлять более одного отзыва на произведение."
+                             "Но можно изменить существующий")
+        return data
