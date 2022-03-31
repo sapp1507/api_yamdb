598c23ef48861e6bced4eb8dead3cc74dc124b5d
Yakov <93822070+potapovjakov@users.noreply.github.com>
2022-04-01 01:12:47 +0300

    Add UniqueConstraint

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -3,6 +3,7 @@ import datetime as dt
 from django.contrib.auth import get_user_model
 from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
+from django.db.models.constraints import UniqueConstraint
 from rest_framework.exceptions import ValidationError
 
 User = get_user_model()
@@ -99,6 +100,12 @@ class Review(models.Model):
     )
 
     class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=('author', 'title'),
+                name='unique_author_title'
+            )
+        ]
         verbose_name = 'Отзыв'
         verbose_name_plural = 'Отзывы'
 
