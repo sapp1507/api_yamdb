62593a627e0d86522cb993644f73c9e71601216b
Nikita Ilin <ilinWall@yandex.ru>
2022-04-01 02:18:54 +0300

    Pull commit

--- script.py
+++ script.py
@@ -5,18 +5,16 @@ from datetime import date
 
 path_to_db = Path('api_yamdb', 'db.sqlite3')
 path_to_file = Path('api_yamdb', 'static', 'data', 'users.csv')
-today_date = str(date.today())
+today_date = date.today()
 
 
 def get_querry(user_data):
-    querry = """INSERT INTO users_user (username, email, role,
-                                bio, first_name, last_name,
-                                password, is_superuser, is_staff,
-                                is_active, date_joined)
-                        VALUES ('{1}','{2}','{3}',
-                                '{4}','{5}','{6}',
-                                '', '0', '0',
-                                '1', {7});""".format(*user_data, today_date)
+    querry = (
+        "INSERT INTO users_user (id, username, email, role,"
+        " bio, first_name, last_name, password, is_superuser, is_staff,"
+        "is_active, date_joined)"
+        "VALUES ({0},'{1}','{2}','{3}','{4}','{5}','{6}',"
+        "'', '0', '0','1', '{7}');").format(*user_data, today_date)
     return querry
 
 
@@ -24,6 +22,7 @@ def main(path_to_file):
     conn = sqlite3.connect(path_to_db)
     cursor = conn.cursor()
     with open(path_to_file, 'r') as csvfile:
+        csvfile.readline()
         reader = csv.reader(csvfile, delimiter=',')
         for row in reader:
             querry = get_querry(row)
