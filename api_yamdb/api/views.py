38c22e101d8e627a5cf3fd24e74226b020e25615
Yakov <93822070+potapovjakov@users.noreply.github.com>
2022-04-01 01:58:01 +0300

    isort

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,5 +1,4 @@
 from django.contrib.auth import get_user_model
-from django.db import models
 from django.db.models import Avg
 from django.shortcuts import get_object_or_404
 from django_filters.rest_framework import DjangoFilterBackend
