b5abc9eb5fba26c3dc1ef529dbaa9b0db3fe7967
Yakov <93822070+potapovjakov@users.noreply.github.com>
2022-03-31 23:27:52 +0300

    Add custom permission fo Review and Comments

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -19,3 +19,23 @@ class AdminOrReadOnly(BasePermission):
         return (request.user.is_authenticated
                 and (request.user.role == 'admin' or request.user.is_superuser)
                 or request.method in SAFE_METHODS)
+
+
+class ReviewCommentPermissions(BasePermission):
+    """
+    Права доступа: Автор отзыва, модератор или администратор.
+    """
+    def has_permission(self, request, view):
+        if view.action in ['list', 'retrieve']:
+            return True
+        return request.user.is_authenticated
+
+    def has_object_permission(self, request, view, obj):
+        if view.action == 'retrieve':
+            return True
+        if view.action in ['partial_update', 'destroy']:
+            if (request.user == obj.author
+                    or request.user.is_admin
+                    or request.user.is_moderator):
+                return True
+            return False
