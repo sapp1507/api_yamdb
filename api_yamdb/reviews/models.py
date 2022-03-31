22072bc25d0998b077a2bf85d9adef0e4630b4e9
Yakov <93822070+potapovjakov@users.noreply.github.com>
2022-04-01 02:27:56 +0300

    Edit CHOICES

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -3,7 +3,6 @@ import datetime as dt
 from django.contrib.auth import get_user_model
 from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
-from django.db.models.constraints import UniqueConstraint
 from rest_framework.exceptions import ValidationError
 
 User = get_user_model()
