e117e490bebc7c4fa306c9d0cb72d7a5f3cca053
Yakov <93822070+potapovjakov@users.noreply.github.com>
2022-04-01 12:50:17 +0300

    Redone strings 'roles' constant

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -1,4 +1,5 @@
 from rest_framework.permissions import SAFE_METHODS, BasePermission
+from users import models
 
 
 class IsAuthorOrReadOnly(BasePermission):
@@ -10,14 +11,14 @@ class AdminPermission(BasePermission):
     def has_permission(self, request, view):
         user = request.user
         return user.is_authenticated and (
-            user.role == 'admin' or user.is_superuser
+            user.role == models.ADMIN or user.is_superuser
         )
 
 
 class AdminOrReadOnly(BasePermission):
     def has_permission(self, request, view):
         return (request.user.is_authenticated
-                and (request.user.role == 'admin' or request.user.is_superuser)
+                and (request.user.role == models.ADMIN or request.user.is_superuser)
                 or request.method in SAFE_METHODS)
 
 
@@ -32,7 +33,7 @@ class ReviewCommentPermissions(BasePermission):
             return True
         if view.action in ['partial_update', 'destroy']:
             if (request.user == obj.author
-                    or request.user.role == 'admin'
-                    or request.user.role == 'moderator'):
+                    or request.user.role == models.ADMIN
+                    or request.user.role == models.MODERATOR):
                 return True
         return False
--- api_yamdb/users/models.py
+++ api_yamdb/users/models.py
@@ -1,6 +1,10 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
+USER = 'user'
+MODERATOR = 'moderator'
+ADMIN = 'admin'
+
 USER_ROLE_CHOICES = (
     ('user', 'Пользователь'),
     ('admin', 'Администратор'),
@@ -13,7 +17,7 @@ class User(AbstractUser):
     bio = models.TextField('Биография', blank=True)
     role = models.CharField(max_length=12,
                             choices=USER_ROLE_CHOICES,
-                            default='user')
+                            default=USER)
     email = models.EmailField(db_index=True, unique=True)
 
     class Meta:
